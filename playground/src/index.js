cab07edd16ef45c91baff4beea971a1c9d8ec2f4
hungbui7690 <hungbui7690@gmail.com>
2023-03-27 11:27:25 +0700

    Code Splitting: Intro & Practice

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,15 +1,19 @@
 /*
-  Public Paths
-  - with the small image, it will be parsed into raw data > it always works 
-  - but for the big image, it will be renamed and put in build/ 
-    > because of that, sometimes it won't work > we need to set build/ as public folder
+  Code Splitting Intro
+  - pic
 
-  (***) inspect the url of the image before and after adding publicPath > we will see the difference
+///////////////////////////////////
+
+  Code Splitting in Practice
+  - pic: what we will build 
+    > when first load, load index.js 
+    > after click > load other file
 
 */
 
-import sum from './sum'
-import './image_viewer'
+// ***
+const button = document.createElement('button')
+button.innerText = 'Click Me'
+button.onclick = () => {}
 
-const total = sum(10, 5)
-console.log(total)
+document.body.appendChild(button)
